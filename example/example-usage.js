4ec35edf3b82cb3e41203dd82da458d2f17aedc2
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-01 13:06:31 +0900

    Add test and debug script

--- example/example-usage.js
+++ example/example-usage.js
@@ -8,6 +8,15 @@ socket.on('connect', () => { /* ... */ })
 socket.on('my:event', () => { /* ... */ })
 socket.on('disconnect', () => { /* ... */ })
 
+// Using call extension
+{
+  co(function * () {
+    // Wait for ack
+    let result = socket.call('my:event', { foo: 'bar' })
+    console.log(result)
+  })
+}
+
 // Using locking extension
 {
   // Resource to lock.
@@ -15,7 +24,7 @@ socket.on('disconnect', () => { /* ... */ })
 
   co(function * () {
     // Start locking
-    // Throws error if another client lock the resrouce
+    // Throws error if another client lock the resource
     yield socket.lock(resource)
 
     // Stop locking
