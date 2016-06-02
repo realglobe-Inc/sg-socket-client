f64f8c1c9a88a7694efd47733e65f3c9c2e77dc1
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-02 13:51:26 +0900

    Version incremented to 1.2.3

--- test/pubsub_extension_test.js
+++ test/pubsub_extension_test.js
@@ -1,14 +1,14 @@
 /**
- * Test case for subscribeExtension.
+ * Test case for pubsubExtension.
  * Runs with mocha.
  */
 'use strict'
 
-const subscribeExtension = require('../lib/extensions/subscribe_extension.js')
+const pubsubExtension = require('../lib/extensions/pubsub_extension.js')
 const assert = require('assert')
 const co = require('co')
 
-describe('subscribe-extension', () => {
+describe('pubsub-extension', () => {
   before(() => co(function * () {
 
   }))
@@ -17,7 +17,7 @@ describe('subscribe-extension', () => {
 
   }))
 
-  it('Subscribe extension', () => co(function * () {
+  it('Pubsub extension', () => co(function * () {
 
   }))
 })
